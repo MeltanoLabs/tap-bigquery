a9d8e2ef7646555afa5bae1b100258e550e898c1
Aaron Phethean <aphethean@matatika.com>
2024-08-31 07:52:18 +0100

    Unit test basic serialisation with float

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,10 +1,12 @@
 """Tests the client BigQueryStream."""
 
-from typing import List
 import unittest
 from unittest import mock
+
+import math
+from typing import List
 from sqlalchemy import create_mock_engine, engine, Inspector, Column
-from sqlalchemy.types import String
+from sqlalchemy.types import String, Float
 
 from singer_sdk._singerlib import Catalog, CatalogEntry
 from tap_bigquery.tap import TapBigQuery
@@ -18,9 +20,9 @@ def dump(sql, *multiparams, **params):
 class MockInspector(Inspector):
     def __init__(
         self,
-        schema_names,
-        table_names,
-        table_columns
+        schema_names:List[str] = None,
+        table_names:List[str] = None,
+        table_columns = None,
     ):
         self.schema_names = schema_names
         self.table_names = table_names
@@ -28,15 +30,21 @@ class MockInspector(Inspector):
 
     def get_schema_names(self)-> List[str]:
         return self.schema_names
-    
+
     def get_table_names(self, schema:str) -> List[str]:
         return self.table_names
-    
+
     def get_view_names(self, schema:str) -> List[str]:
         raise NotImplementedError
-    
-    def get_columns(self, table_name: str, schema_name: str) -> dict[str, Column]:
-        return self.table_columns[schema_name + '.' + table_name]
+
+    def get_indexes(self, table_name: str, schema: str) -> List[str]:
+        return []
+
+    def get_columns(self, table_name: str, schema: str) -> dict[str, Column]:
+        return self.table_columns[schema + '.' + table_name]
+
+    def get_pk_constraint(self, table_name: str, schema: str) -> dict:
+        return {}
 
 
 class TestClient(unittest.TestCase):
@@ -49,21 +57,8 @@ class TestClient(unittest.TestCase):
         # default catalog setup discovers streams
         self.mock_catalog = None
 
-    # Run standard built-in tap tests from the SDK:
     @mock.patch("sqlalchemy.create_engine", return_value=create_mock_engine('bigquery://mockprojectid', dump))
-    @mock.patch("sqlalchemy.inspect", return_value=MockInspector(
-            'mock-schema', 
-            'mock_table',
-            {
-                'mock-schema.mock_table': [
-                    {
-                        'name': 'string_field',
-                        'type': String(50),
-                    }
-                ],
-            }
-        )
-    )
+    @mock.patch("sqlalchemy.inspect", return_value=MockInspector())
     @mock.patch("singer_sdk.SQLStream.get_records", return_value=mock_records)
     def test_record_with_none_float_serialisable(self, mock_engine, mock_inspector, mock_sql_tap):
         # given a mock DB schema
@@ -85,6 +80,24 @@ class TestClient(unittest.TestCase):
                                         "null",
                                     ]
                                 },
+                                "float_field":{
+                                    "type":[
+                                        "float_value",
+                                        "null",
+                                    ]
+                                },
+                                # "float_none":{
+                                #     "type":[
+                                #         "float_value",
+                                #         "null",
+                                #     ]
+                                # },
+                                # "float_infinity":{
+                                #     "type":[
+                                #         "float_value",
+                                #         "null",
+                                #     ]
+                                # },
                             },
                         },
                     }
@@ -96,7 +109,12 @@ class TestClient(unittest.TestCase):
             catalog=self.mock_catalog,
         )
         # given an sqlachemy result set contains floats with None values
-        self.mock_records.append({"string_field": "jam"})
+        self.mock_records.append({
+            "string_field": "jam",
+            "float_field": 0.0,
+            # "float_none": None,
+            # "float_infinity": math.inf,
+        })
         # when get_records
         self.assertEqual(len(tap.streams), 1)
         records = tap.streams['mock_table'].get_records(partition = None)
@@ -104,7 +122,42 @@ class TestClient(unittest.TestCase):
         # expect the result can be serialised by simplejson
         for record in records:
             json_output = BigQueryConnector().serialize_json(record)
+            self.assertEqual(json_output, '{"string_field":"jam","float_field":0.0}')
+
+
+    @mock.patch("sqlalchemy.create_engine", return_value=create_mock_engine('bigquery://mockprojectid', dump))
+    @mock.patch("sqlalchemy.inspect", return_value=MockInspector(
+            ['mock-schema'], 
+            ['mock_table'],
+            {
+                'mock-schema.mock_table': [
+                    { 'name': 'string_field', 'type': String(50) },
+                ],
+            },
+        )
+    )
+    @mock.patch("singer_sdk.SQLStream.get_records", return_value=mock_records)
+    def test_catalog_discovery(self, mock_engine, mock_inspector, mock_sql_tap):
+        # given a mock DB schema
+        # given a mock DB table
+        # given a tap instance with catalog discovered from sqlalchemy
+        tap = TapBigQuery(
+            config=self.mock_config,
+            catalog=self.mock_catalog,
+        )
+        # given an sqlachemy result set contains floats with None values
+        self.mock_records.append({
+            "string_field": "jam",
+        })
+        # when get_records
+        self.assertEqual(len(tap.streams), 1)
+        records = tap.streams['mock-schema-mock_table'].get_records(partition = None)
+
+        # expect the result can be serialised by simplejson
+        for record in records:
+            json_output = BigQueryConnector().serialize_json(record)
             self.assertEqual(json_output, '{"string_field":"jam"}')
-            
+
+
 
 
